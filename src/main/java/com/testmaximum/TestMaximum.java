084c4acfa4053fbaf43bf867b91504fce01880c2
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-06 20:43:49 +0530

    Added List

--- src/main/java/com/testmaximum/TestMaximum.java
+++ src/main/java/com/testmaximum/TestMaximum.java
@@ -1,33 +1,102 @@
 package com.testmaximum;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.lang.Comparable;
 
-public class TestMaximum {
+public class TestMaximum<T extends Comparable<T>>
+{
  
-public static void main(String[] args)
-  {
-    @SuppressWarings("unchecked")
-    List<Integer> integerList=new ArrayList<>(List.of(3,2,1,4,5));
-    List<String> stringList=new ArrayList<>(List.of("H","A","S","I"));
-    List<String> floatList=new ArrayList<>(List.of("5.1","4.1","3.1","1.1"));
- 
-    sortArrays(integerList,stringList,floatList);
-    System.out.println(integerList);
-    System.out.println(stringList);
-    System.out.println(floatList);
-  
-  }
-/**
- * This function is used to sort the arraylist using wildcard objects
- * @param arr
- */
+	    T num1;
+	    T num2;
+	    T num3;
 
-private static void sortArrays(List<? extends Comparable>... arr) {
-	for (List<? extends Comparable> list : arr) {
-        Collections.sort(list);
-        System.out.println(Collections.max(list));
-	
-  }    
+	    public TestMaximum(T num1, T num2, T num3) {
+	        this.num1 = num1;
+	        this.num2 = num2;
+	        this.num3 = num3;
+	    }
+
+	    public TestMaximum() {
+	    }
+
+	    /**
+	     * UC 1: method for finding maximum integer value
+	     */
+	    public static Integer findMax(Integer num1, Integer num2, Integer num3) {
+	        Integer max = num1;
+	        if (num2.compareTo(max) > 0) {
+	            max = num2;
+	        }
+	        if (num3.compareTo(max) > 0) {
+	            max = num3;
+	        }
+	        return max;
+	    }
+
+	    /**
+	     * UC 2:  method for finding maximum float value
+	     */
+	    public static Float findMax(Float num1, Float num2, Float num3) {
+	        Float max = num1;
+	        if (num2.compareTo(max) > 0) {
+	            max = num2;
+	        }
+	        if (num3.compareTo(max) > 0) {
+	            max = num3;
+	        }
+	        return max;
+	    }
+
+	    /**
+	     * UC 3:  method for finding maximum String value
+	     */
+	    public static String findMax(String str1, String str2, String str3) {
+	        String max = str1;
+	        if (str2.compareTo(max) > 0) {
+	            max = str2;
+	        }
+	        if (str3.compareTo(max) > 0) {
+	            max = str3;
+	        }
+	        return max;
+	    }
+
+	    /**
+	     * using generics find Maximum value of String
+	     */
+	    public static <T extends Comparable<T>> T findAnyMax(T num1, T num2, T num3) {
+	        T max = num1;
+	        if (num2.compareTo(max) > 0)
+	            max = num2;
+	        if (num3.compareTo(max) > 0)
+	            max = num3;
+	        return max;
+	    }
+
+	    public T findAnyMax() {
+	        return TestMaximum.findAnyMax(num1, num2, num3);
+	    }
+
+	    /**
+	     * UC4: method to take more than three parameters
+	     */
+	    public static <T extends Comparable<T>> T findAnyMax(T... elements) {
+	        List<T> list = (List<T>) Arrays.asList(elements).stream().sorted().collect(Collectors.toList());
+	        return list.get(elements.length - 1);
+	    }
+
+	    /**
+	     * main function
+	     *
+	     * @param args
+	     */
+	    public static void main(String[] args) {
+	        System.out.println("Maximum Value of Integer given is :" + findMax(6, 3, 9));
+	        System.out.println("Maximum value of Float numbers given is :" + findMax(1.0f, 5.0f, 3.0f));
+	        System.out.println("Maximum value of String given is :" + findMax("Boy", "Girl", "Animal"));
+	        System.out.println("Maximum value of String given is :" + findAnyMax("Boy", "Girl", "Animal"));
+	        System.out.println("Maximum Integer by creating Generic class :" + new TestMaximum<Integer>(3, 6, 2).findAnyMax());
+	    }    
  }
-}
+
  
--- src/test/java/com/testmaximum/FindMaximumTest.java
+++ src/test/java/com/testmaximum/FindMaximumTest.java
@@ -1,100 +1,135 @@
 package com.testmaximum;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
+
 public class FindMaximumTest {
-private TestMaximum testMaximum;
-int maximumNumber;
+
+	/**
+     * UC1.1, UC1.2 and UC1.3
+     */
+    @Test
+    public void MaxValueAtFirstPosition() {
+        Integer max = TestMaximum.findMax(9,3,2);
+        Assert.assertSame(9, max);
+    }
+
+    @Test
+    public void MaxValueAtSecondPosition() {
+        Integer max = TestMaximum.findMax(3,7,2);
+        Assert.assertSame(7, max);
+    }
+
+    @Test
+    public void MaxValueAtThirdPosition() {
+        Integer max = TestMaximum.findMax(2,3,8);
+        Assert.assertSame(8, max);
+    }
+
+    /**
+     * testcase when all elements have same value
+     */
+    @Test
+    public void CheckMaxWhenAllElementAreSame() {
+        Integer max = TestMaximum.findMax(9,9,9);
+        Assert.assertSame(9, max);
+    }
 
 
-/**
- * Function to test if first integer position is maximum
- */
-@Test
-public void firstPosition_ifMaximum_returnFirstPosition()
-{
-	testMaximum= new TestMaximum(2,3,1);
-	int maximumNumber=(int)testMaximum.testMaximum();
-	Assert.assertEquals(3, maximumNumber);
-	
-}
+    /**
+     * UC2..testcases
+     */
+    @Test
+    public void firstFloatMaxValueReturnFirst() {
+        Float max = TestMaximum.findMax(9.01f, 6.01f, 3.01f);
+        Assert.assertThat(max, CoreMatchers.equalTo(9.01f));
+    }
 
-/**
- * Function to test if second integer position is maximum
- */
-@Test
-public void secondPosition_ifMaximum_returnsThirdPosition()
-{
-	testMaximum = new TestMaximum(2,3,1);
-    int maximumNumber = (int)testMaximum.testMaximum();
-    Assert.assertEquals(3, maximumNumber);
-}
-/**
- * Function to test if third integer position is maximum
- */
-@Test
-public void thirdPosition_ifMaximum_returnsThirdPosition() {
-    testMaximum = new TestMaximum(2,3,1);
-    int maximumNumber =(int)testMaximum.testMaximum();
-    Assert.assertEquals(3, maximumNumber);
-}
-/**
- * Function to test if first float position is maximum
- */
-@Test
-public void firstFloatPosition_ifMaximum_returnsFirstPosition()
-{
-	TestMaximum maximum=new TestMaximum(2,3,1);
-	int max=(int)maximum.testMaximum();
-	Assert.assertEquals(3,max,0.01);
-}
+    @Test
+    public void secondFloatMaxValueReturnSecond() {
+        Float max = TestMaximum.findMax(3.01f, 9.01f, 6.01f);
+        Assert.assertThat(max, CoreMatchers.equalTo(9.01f));
+    }
 
-/**
- * Function to test if second float position is maximum
- */
-@Test
-public void secondFloatPosition_ifMaximum_returnsSecondPosition() {
-	TestMaximum maximum=new TestMaximum(2,3,1);
-	int max=(int)maximum.testMaximum();
-    Assert.assertEquals(3,max,0.01);
-}
+    @Test
+    public void thirdFloatMaxValueReturnThird() {
+        Float max = TestMaximum.findMax(6.01f, 3.01f, 9.01f);
+        Assert.assertThat(max, CoreMatchers.equalTo(9.01f));
+    }
 
-/**
- * Function to test if third float position is maximum
- */
-@Test
-public void thirdFloatPosition_ifMaximum_returnsThirdPosition() {
-	TestMaximum maximum=new TestMaximum(2,3,1);
-	int max=(int)maximum.testMaximum();
-    Assert.assertEquals(3, max, 0.01);
-}
+    /**
+     * UC3: testcases..
+     */
+    @Test
+    public void firstStringMaxReturnFirst() {
+        String max = TestMaximum.findMax("Girl","Boy","Animal");
+        Assert.assertThat(max, CoreMatchers.equalTo("Girl"));
+    }
 
-/**
- * Function to test if first string position is maximum
- */
-@Test
-public void firstStringPosition_ifMaximu_returnsFirstPosition()
-{
-	TestMaximum maximum=new TestMaximum("Apple","Peach","Banana");
-	String max=(String)maximum.testMaximum();
-	Assert.assertEquals("Peach",max);
-	
-}
-@Test
-public void secondStringPosition_ifMaximum_returnsSecondPosition() {
-    TestMaximum maximum = new TestMaximum("Apple", "Peach", "Banana");
-    String max = (String) maximum.testMaximum();
-    Assert.assertEquals("Peach", max);
-}
-/**
- * Function to test if third string position is maximum
- */
-@Test
-public void thirdStringPosition_ifMaximum_returnsThirdPosition() {
-    TestMaximum maximum = new TestMaximum("Apple", "Peach", "Banana");
-    String max = (String) maximum.testMaximum();
-    Assert.assertEquals("Peach", max);
-}
+    @Test
+    public void secondStringMaxReturnSecond() {
+        String max = TestMaximum.findMax("Boy","Girl","Animal");
+        Assert.assertThat(max, CoreMatchers.equalTo("Girl"));
+    }
+
+    @Test
+    public void thirdStringMaxReturnThird() {
+        String max =TestMaximum.findMax("Boy","Animal","Girl");
+        Assert.assertThat(max, CoreMatchers.equalTo("Girl"));
+    }
+
+    /**
+     * After refactor the code : testcases
+     */
+    @Test
+    public void ReturnMaxFloat() {
+        Float max = TestMaximum.findAnyMax(9.01f, 6.01f, 3.01f);
+        Assert.assertThat(max, CoreMatchers.equalTo(9.01f));
+    }
+    @Test
+    public void ReturnMaxString() {
+        String max = TestMaximum.findAnyMax("Boy","Girl","Animal");
+        Assert.assertThat(max, CoreMatchers.equalTo("Girl"));
+    }
+    @Test
+    public void ReturnMaxInteger() {
+        Integer max = TestMaximum.findAnyMax(1, 2, 3);
+        Assert.assertThat(max, CoreMatchers.equalTo(3));
+    }
+
+    /**
+     * testcases for generics class
+     */
+    @Test
+    public void anyTypeReturnMaxInteger() {
+        Integer max = new TestMaximum<Integer>(1,2,3).findAnyMax();
+        Assert.assertThat(max, CoreMatchers.equalTo(3));
+    }
+    @Test
+    public void anyTypeReturnMaxFloat() {
+        Float max = new TestMaximum<Float>(1.01f, 2.02f, 3.03f).findAnyMax();
+        Assert.assertThat(max, CoreMatchers.equalTo(3.03f));
+    }
+    @Test
+    public void anyTypeReturnMaxString() {
+        String max = new TestMaximum<String>("Boy", "Girl", "Animal").findAnyMax();
+        Assert.assertThat(max, CoreMatchers.equalTo("Girl"));
+    }
+
+    /**
+     * find max more than 3 variables in parameters
+     */
+    @Test
+    public void IntegerMaxReturnThirdMultiParameter() {
+        Integer max = TestMaximum.findAnyMax(5,3,2,6,7,1,9);
+        Assert.assertThat(max, CoreMatchers.equalTo(9));
+    }
+    @Test
+    public void FloatMaxReturnThirdMultiParameter() {
+        Float max = TestMaximum.findAnyMax(1.0f,5.0f,6.0f,3.0f,7.0f);
+        Assert.assertThat(max, CoreMatchers.equalTo(7.0f));
+    }
 }
 
 
