fdac8cac4c230d8184de08e2f266b81420a5724c
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-06 17:42:56 +0530

    Added float numbers

--- src/main/java/com/testmaximum/TestMaximum.java
+++ src/main/java/com/testmaximum/TestMaximum.java
@@ -24,4 +24,18 @@ public class TestMaximum {
         }
         return max;
     }
+    public Float findMaximumFloat(Float num1,Float num2,Float num3)
+    {
+    	Float max=num1;
+    	if(num2.compareTo(max)>0)
+    	{
+    		max=num2;
+    	}
+    	if(num3.compareTo(max)>0)
+    	{
+    		max=num3;
+    	}
+		return max;
+    	
+    }
 }
